3048617a3a87bd13afa247ef8e9d0cb04e5f46f7
devMalaikaKhan <developermalaikakhan@gmail.com>
2025-11-11 15:45:52 +0500

    first commit

---
+++ chap no 2/index.js
@@ -0,0 +1,7 @@
+var name="MalaikaKhan"
+var myVariable = "This is my chosen string.";
+console.log(myVariable);
+var nickname="NewName"
+alert(nickname);
+var bestMan = "Charlie";
+bestMan = "David";
---
+++ chp no1/app.js
@@ -0,0 +1,7 @@
+alert(" first name: Malaika");
+alert(" last name: Khan"); 
+alert("email: malaika.khan@example.com");
+alert("phone: +91 9876543210");
+alert("password: ********");
+alert("youre learnig javascript");
+alert("interested in web development");
